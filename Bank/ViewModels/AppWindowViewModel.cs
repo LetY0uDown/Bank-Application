a82fb34c0616ff2268872b97f228c9ef7acf72b4
Maksim <maks.manin.04@gmail.com>
2022-04-30 10:37:08 +1000

    Изменения

--- Bank/ViewModels/AppWindowViewModel.cs
+++ Bank/ViewModels/AppWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Bank.Core.Objects.Abstract;
+﻿using Bank.Core.Objects;
+using Bank.Core.Objects.Abstract;
+using Bank.Views.Pages;
 using Bank.Models;
 using System.Windows.Controls;
 
@@ -6,7 +8,15 @@ namespace Bank.ViewModels;
 
 public sealed class AppWindowViewModel : ObservableObject
 {
+    public AppWindowViewModel()
+    {
+        ShowSettingsCommand = new(o =>
+            CurrentPage = new SettingsPage());
+    }
+
     public User CurrentUser => App.CurrentUser!;
+
+    public Command ShowSettingsCommand { get; }
 
     public Page? CurrentPage { get; set; }
 }
