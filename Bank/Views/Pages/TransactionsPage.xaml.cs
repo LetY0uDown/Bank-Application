7d122dce3cd84a29eefc4aff14643d4458f283f5
Maksim <93037214+LetY0uDown@users.noreply.github.com>
2022-05-18 09:35:02 +1000

    Исправлен баг
    Исправлен баг с возможностью ввода отрицательных чисел в поле суммы

--- Bank/Views/Pages/TransactionsPage.xaml.cs
+++ Bank/Views/Pages/TransactionsPage.xaml.cs
@@ -2,4 +2,12 @@
 
 namespace Bank.Views.Pages;
 
-public sealed partial class TransactionsPage : Page { public TransactionsPage() => InitializeComponent(); }
+public sealed partial class TransactionsPage : Page 
+{ 
+    public TransactionsPage() => InitializeComponent();
+
+    private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+    {
+        if (e.Text[0].Equals('-')) e.Handled = true;
+    }
+}
