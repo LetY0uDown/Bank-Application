0a012386544955e4ba9d77845de76d828f4ce211
Maksim <93037214+LetY0uDown@users.noreply.github.com>
2022-05-18 10:50:04 +1000

    Update TransactionsPage.xaml.cs

--- Bank/Views/Pages/TransactionsPage.xaml.cs
+++ Bank/Views/Pages/TransactionsPage.xaml.cs
@@ -5,9 +5,10 @@ namespace Bank.Views.Pages;
 public sealed partial class TransactionsPage : Page 
 { 
     public TransactionsPage() => InitializeComponent();
+    private static readonly Regex _numberRegex = new("^[0-9]*$");
 
     private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
     {
-        if (e.Text[0].Equals('-')) e.Handled = true;
+        if (!_numberRegex.IsMatch(e.Text)) e.Handled = true;
     }
 }
